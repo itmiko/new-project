2a4b7e9bc7f33fe70287155372f2020ef472760c
itmiko <mypost.mk@gmail.com>
2015-09-30 23:52:15 +0300

    add new_p

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	rename = require('gulp-rename'),
+	notify = require("gulp-notify"),
+	// autoprefixer = require('gulp-autoprefixer'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect'),
+	minifyCss = require('gulp-minify-css');
+
+
+//server connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+//css
+gulp.task('css', function () {
+  	gulp.src('css/*.css')
+    .pipe(concatCss("bundle.css"))
+    // .pipe(autoprefixer({browsers: ['last 3 versions']}))
+    .pipe(minifyCss())
+    .pipe(rename("bundle.min.css"))
+    .pipe(gulp.dest('app/css'))
+    // .pipe(notify("Done!"))
+    .pipe(connect.reload());
+});
+
+//html
+gulp.task('html', function(){
+	gulp.src('app/index.html')
+    .pipe(connect.reload());
+});
+
+//watch
+gulp.task('watch', function () {
+  	gulp.watch('css/*.css', ['css'])
+  	gulp.watch('app/index.html', ['html'])
+
+});
+
+//default
+gulp.task('default', ['connect', 'html', 'css', 'watch']);//use this task when type "gulp" in consol 
